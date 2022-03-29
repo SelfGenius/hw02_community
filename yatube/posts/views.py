558136c06652b7ab0209509d817025eea089d512
Александр Гетманов <getmanov.wyrik@yandex.ru>
2022-03-29 22:59:38 +0300

    add content.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-
-from .models import Post, Group
+from django.shortcuts import get_object_or_404, render
 
 from yatube.settings import NUMBER_OF_ENTRIES
+
+from .models import Group, Post
 
 
 def index(request):
