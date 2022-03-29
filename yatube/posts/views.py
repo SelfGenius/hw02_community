c40985603f1fc8199f2ac0bcb06aabf3f8df78da
Александр Гетманов <getmanov.wyrik@yandex.ru>
2022-03-29 09:58:45 +0300

    all()

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -16,7 +16,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author').all
+    posts = group.posts.select_related('author').all()
     title = 'Лев Толстой – зеркало русской революции.'
     context = {
         'group': group,
